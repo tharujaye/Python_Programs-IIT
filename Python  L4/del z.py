f4f8fb484dd6e2e786964d0795c2a10a10abf8f4
Tharusha Jayawardena <121338079+tharujaye@users.noreply.github.com>
2023-10-10 16:02:31 +0530

    Add files via upload

---
+++ Python  L4/del z.py
@@ -0,0 +1,3 @@
+n = int(input('Give me a number over 100: '))
+if n <= 100:
+    Print(n, 'is not over 100')
---
+++ Python  L4/test1.py
@@ -0,0 +1,112 @@
+#3)
+#a)
+num = 5
+print(num) #Output?
+num = num + 2
+print(num) #Output?
+num = num // 3 * 6
+print(num) #Output?
+print(7 + 15 % 4) #Output?
+num = 24 // 3 // 4
+print(num) #Output?
+
+
+#b) i
+a = "Hello out there."
+print(a)
+b = 'Hello again.'
+print(b)
+
+#b) ii
+total = '10'
+greet = 'Hello'
+both = total + greet
+print(both)
+
+#b) iii
+print("A", end = ' ')
+print("B", end = ' ')
+print("C", end = ' ')
+print("D", end = ' ')
+
+#b) iv
+a = '10'
+b = '99'
+c = a + b
+print(c)
+
+#mod
+#"\n"
+a = int(10)
+b = int(99)
+c = (a + b)
+print(c)
+
+#4)
+#i
+name = input('Please enter your name: \n')
+print('Hello', name)
+
+#ii
+age = input('Please eneter your age: \n')
+print('your age is', age)
+
+#iii
+print("test\\test2\\answers.txt")
+
+#iv
+the_text = input('Enter some text.\n') # get some text!
+#print - version 1
+print('This is what you entered: ')
+print(the_text)
+#print – version 2
+print('This is what you entered:', the_text)
+#print - version 3 - Supress printing of a new line, use end=' '
+print('This is what you entered:', end=' ')
+print(the_text)
+
+#v
+num_1=int(input('Enter number 1: '))
+num_2=int(input('Enter number 2: '))
+total=num_1+num_2
+print(total)
+
+#
+cost = float(input('Enter cost of item: '))
+paid = float(input('Enter cash paid: '))
+change = paid-cost
+print('Change is: ', change)
+
+#
+num1=int(input('Enter number 1 :'))
+num2=int(input('Enter number 2 : '))
+num3=int(input('Enter number 3 : '))
+average=(num1+num2+num3)/3
+print(average)
+
+# 5)
+#
+# shopping program with 4 bugs
+a=3 # number of apples
+o=0.0 # number of oranges
+print('you have: ',a,'oranges and',o,'apples')
+buy_a=4 # apples you buy
+buy_o=6 # oranges you buy
+print('you buy: ',buy_a,'apples and',buy_o,'oranges')
+a=a+buy_A
+o=o+buy_o
+PRINT('you now have:',a,'apples and',o,'oranges')
+
+#
+
+# shopping program with 4 bugs
+a=3       # number of apples
+o=0     # number of oranges
+print('you have:    ',a,'oranges and',o,'apples')
+buy_a=4   # apples you buy
+buy_o=6 # oranges you buy
+print('you buy:     ',buy_a,'apples and',buy_o,'oranges')
+a=a+buy_a
+o=o+buy_o
+print('you now have:',a,'apples and',o,'oranges')
+
